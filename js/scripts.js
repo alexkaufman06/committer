7b479ddbffe42ad357365c175667a5333f1cf45e
Alex Kaufman <alexkaufman06@gmail.com>
2015-08-30 23:06:25 -0700

    Add scripts and commit functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
-// future scripts here
+var Commiter = function (title) {
+  this.title = title;
+  this.completed = ko.observable(false);
+  this.className = ko.computed(function () {
+    return this.completed() ? 'completed' : null;
+  }, this);
+};
+
+var Commits = function () {
+  this.commits = ko.observableArray([]);
+  this.commitToAdd = ko.observable('');
+  this.addCommit = function () {
+    this.commits.push(new Commiter(this.commitToAdd()));
+    this.commitToAdd('');
+  };
+};
+
+ko.applyBindings(new Commits);
